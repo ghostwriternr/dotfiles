77ebad958fc8d5650dbfeb67e4c7c03f665bd8a0
Naresh <naresh@cloudflare.com>
2026-02-24 17:56:41 +0000

    add home-manager integration

--- flake.nix
+++ flake.nix
@@ -1,13 +1,15 @@
 {
-  description = "Example nix-darwin system flake";
+  description = "Naresh's darwin system";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:nix-darwin/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
   let
     configuration = { pkgs, ... }: {
       # List packages installed in system profile. To search by name, run:
@@ -19,9 +21,6 @@
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
 
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
-
       # Set Git commit hash for darwin-version.
       system.configurationRevision = self.rev or self.dirtyRev or null;
 
@@ -31,13 +30,25 @@
 
       # The platform the configuration will be used on.
       nixpkgs.hostPlatform = "aarch64-darwin";
+
+      # The user that owns the system.
+      system.primaryUser = "naresh";
     };
   in
   {
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#KVQ52GY6N9
     darwinConfigurations."KVQ52GY6N9" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+      modules = [
+        configuration
+        home-manager.darwinModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.backupFileExtension = "backup";
+          home-manager.users.naresh = import ./home.nix;
+        }
+      ];
     };
   };
 }
---
+++ home.nix
@@ -0,0 +1,7 @@
+{ pkgs, ... }:
+
+{
+  home.username = "naresh";
+  home.homeDirectory = "/Users/naresh";
+  home.stateVersion = "25.11";
+}
